38963b2319c88621082557276b100c0bac018c3f
Miroslav Šuvada <miroslav.suvada@gmail.com>
2023-02-27 13:09:00 +0100

    Reformat nsone-reports.py

--- nsone-reports.py
+++ nsone-reports.py
@@ -49,7 +49,8 @@ def parse_args():
     )
     parser_export = parser.add_argument_group()
     parser_export.add_argument(
-        "--export", "-e",
+        "--export",
+        "-e",
         action="store_true",
         help="export report in same format as in NS1 gui",
     )
@@ -60,14 +61,18 @@ def parse_args():
         help="filetype of generated report (default: %(default)s)",
     )
     parser_mail = parser.add_argument_group()
-    parser_mail.add_argument("--mailfrom", default=None, type=str, help="sender address")
+    parser_mail.add_argument(
+        "--mailfrom", default=None, type=str, help="sender address"
+    )
     parser_mail.add_argument(
         "--mailto",
         default=None,
         type=str,
         help="recipient address separated by comma (default: %(default)s)",
     )
-    parser.add_argument("--silent", "-s", action="store_true", help="don't print report to stdout")
+    parser.add_argument(
+        "--silent", "-s", action="store_true", help="don't print report to stdout"
+    )
     return parser.parse_args()
 
 
@@ -132,7 +137,11 @@ def parse_reports(reports):
                 if item["resource"].get("domain"):
                     result["domain"] = item["resource"].get("domain")
                 if item["resource"].get("answers"):
-                    result["answers"] = list(chain.from_iterable([i["answer"] for i in item["resource"]["answers"]]))
+                    result["answers"] = list(
+                        chain.from_iterable(
+                            [i["answer"] for i in item["resource"]["answers"]]
+                        )
+                    )
                 else:
                     result["resource"] = item.get("resource")
                 output.append(yaml.dump(result, sort_keys=False, allow_unicode=True))
@@ -144,9 +153,11 @@ def parse_reports(reports):
     return "".join(output)
 
 
-def send_mail(mailfrom, mailto, server, user, password, body, formatting=None, port=465):
+def send_mail(
+    mailfrom, mailto, server, user, password, body, formatting=None, port=465
+):
     toaddrs = mailto.split(",")
-    message = MIMEMultipart('alternative')
+    message = MIMEMultipart("alternative")
     message["Subject"] = "NS1 Activity Report"
     message["From"] = mailfrom
     message["To"] = mailto
@@ -158,11 +169,15 @@ def send_mail(mailfrom, mailto, server, user, password, body, formatting=None, p
     else:
         plain_part = MIMEText(body, "plain", "utf-8")
         env = Environment(
-            loader=FileSystemLoader(Path(__file__).resolve().parent.joinpath("templates")),
-            autoescape=select_autoescape(['html'])
+            loader=FileSystemLoader(
+                Path(__file__).resolve().parent.joinpath("templates")
+            ),
+            autoescape=select_autoescape(["html"]),
         )
-        template = env.get_template('nsone-report.html.j2')
-        html_part = MIMEText(template.render(body=body, title="NS1 Activity Report"), "html", "utf-8")
+        template = env.get_template("nsone-report.html.j2")
+        html_part = MIMEText(
+            template.render(body=body, title="NS1 Activity Report"), "html", "utf-8"
+        )
         message.attach(plain_part)
         message.attach(html_part)
     server = smtplib.SMTP_SSL(server, port)
@@ -179,14 +194,16 @@ def send_mail(mailfrom, mailto, server, user, password, body, formatting=None, p
 if __name__ == "__main__":
     args = parse_args()
     config = parse_config(args.config)
-    log_name = Path(__file__).name.replace('.py', '.log')
-    log_dir = Path(config.get('nsone', 'NSONE_LOGDIR')).expanduser()
+    log_name = Path(__file__).name.replace(".py", ".log")
+    log_dir = Path(config.get("nsone", "NSONE_LOGDIR")).expanduser()
     log_path = log_dir.joinpath(log_name)
     try:
-        logging.basicConfig(format='%(time)s | %(levelname)-8s | %(message)s',
-                            filename=log_path,
-                            filemode='a',
-                            level=logging.DEBUG)
+        logging.basicConfig(
+            format="%(time)s | %(levelname)-8s | %(message)s",
+            filename=log_path,
+            filemode="a",
+            level=logging.DEBUG,
+        )
     except FileNotFoundError as e:
         logging.error(f"Unable to open {log_path}")
         sys_exit(e)
@@ -209,19 +226,27 @@ if __name__ == "__main__":
 
     # TODO save exports to file
     if not args.mailto or args.silent:
-        print(parsed_reports if not args.export else parsed_reports.decode('utf-8'))
+        print(parsed_reports if not args.export else parsed_reports.decode("utf-8"))
 
     if args.mailto and len(parsed_reports) != 0:
         mail_params = {
-            'header': args.header if args.header else config.get("mail", "header", fallback=None),
-            'footer': args.footer if args.footer else config.get("mail", "footer", fallback=None),
-            'mailfrom': args.mailfrom if args.mailfrom else config.get("smtp", "from", fallback="localhost"),
-            'mailto': args.mailto if args.mailto else config.get("smtp", "mailto", fallback=None),
-            'server': config.get("smtp", "server", fallback="localhost"),
-            'user': config.get("smtp", "username", fallback=None),
-            'password': config.get("smtp", "password", fallback=None),
-            'body': parsed_reports,
-            'formatting': args.format if args.export else None,
-            'port': config.getint("smtp", "port", fallback=465),
+            "header": args.header
+            if args.header
+            else config.get("mail", "header", fallback=None),
+            "footer": args.footer
+            if args.footer
+            else config.get("mail", "footer", fallback=None),
+            "mailfrom": args.mailfrom
+            if args.mailfrom
+            else config.get("smtp", "from", fallback="localhost"),
+            "mailto": args.mailto
+            if args.mailto
+            else config.get("smtp", "mailto", fallback=None),
+            "server": config.get("smtp", "server", fallback="localhost"),
+            "user": config.get("smtp", "username", fallback=None),
+            "password": config.get("smtp", "password", fallback=None),
+            "body": parsed_reports,
+            "formatting": args.format if args.export else None,
+            "port": config.getint("smtp", "port", fallback=465),
         }
         send_mail(**mail_params)
